7d62d1a66b7366b05be3ad44ed25fcc6e22915d1
Daniel Siegle <daniel.siegle@gmail.com>
2023-02-22 16:00:04 +0000

    improved install and added rudimentary error handling

--- lox.java
+++ lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  static boolean hadError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -25,7 +26,11 @@ public class Lox {
 private static void runFile(String path) throws IOException {
   byte[] bytes = Files.readAllBytes(Paths.get(path));
   run(new String(bytes, Charset.defaultCharset()));
+
+  // Indicate an error n the exit code.
+  if (hadError) System.exit(65);
 }
+
 
 private static void runPrompt() throws IOException {
   InputStreamReader input = new InputStreamReader(System.in);
@@ -38,6 +43,7 @@ private static void runPrompt() throws IOException {
     run(line);
   }
 
+
 private static void run(String source) {
   Scanner scanner = new Scanner(source);
   List<Token> tokens = scanner.scanTokens();
@@ -48,3 +54,15 @@ private static void run(String source) {
   }
 }
 
+
+static void error(int line, String message) {
+  report(line, "", message);
+}
+
+
+private static void report(int line, String where,
+                           String message) {
+  System.err.println(
+      "[line " + line + "] Error" + where + ": " + message);
+  hadError = true;
+}
