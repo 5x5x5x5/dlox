931bb45d8b682d817929e22e06b6ce59d8c35453
Daniel Siegle <daniel.siegle@gmail.com>
2023-02-28 15:21:12 +0000

    fixed indent

--- lox.java
+++ lox.java
@@ -20,49 +20,48 @@ public class Lox {
       runPrompt();
     }
   }
-}
 
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
 
-private static void runFile(String path) throws IOException {
-  byte[] bytes = Files.readAllBytes(Paths.get(path));
-  run(new String(bytes, Charset.defaultCharset()));
-
-  // Indicate an error n the exit code.
-  if (hadError) System.exit(65);
-}
-
-
-private static void runPrompt() throws IOException {
-  InputStreamReader input = new InputStreamReader(System.in);
-  BufferedReader reader = new BufferedReader(input);
-
-  for (;;) {
-    System.out.pring("> ");
-    String line = reader.readLine();
-    if (line == null) break;
-    run(line);
+    // Indicate an error n the exit code.
+    if (hadError) System.exit(65);
   }
 
 
-private static void run(String source) {
-  Scanner scanner = new Scanner(source);
-  List<Token> tokens = scanner.scanTokens();
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
 
-  // For now, just print the tokens.
-  for (Token token : token) {
-    System.out.println(token);
+    for (;;) {
+      System.out.pring("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+    }
+
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : token) {
+      System.out.println(token);
+    }
   }
-}
 
 
-static void error(int line, String message) {
-  report(line, "", message);
-}
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
 
 
-private static void report(int line, String where,
-                           String message) {
-  System.err.println(
-      "[line " + line + "] Error" + where + ": " + message);
-  hadError = true;
+  private static void report(int line, String where,
+                              String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
 }
